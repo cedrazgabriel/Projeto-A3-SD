57ba8715557602f1745b88feb81a0fff5463e2fd
Harry Potter <cedrazdev@gmail.com>
2023-12-08 22:56:13 -0300

    adiciona documentação com swagger

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
 const clienteRoute = require('./routes/clienteRoutes.js');
 const categoriaRoute = require('./routes/categoriaRoutes.js');
@@ -15,6 +17,22 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
+// Defina as opções do Swagger JSDoc
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'API de Vendas (Sistemas Distribuídos)',
+            version: '1.0.0',
+        },
+    },
+    // Caminho para os arquivos de rota que você deseja documentar
+    apis: ['./src/routes/*.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use('/clientes', clienteRoute);
 app.use('/categorias', categoriaRoute);
--- src/routes/clienteRoutes.js
+++ src/routes/clienteRoutes.js
@@ -5,10 +5,166 @@ const { validateGetById, checkValidationResultId } = require('../validators/quer
 
 const clienteRoutes = express.Router();
 
+/**
+ * @swagger
+ * tags:
+ *   - name: Clientes
+ *     description: Operações relacionadas a clientes
+ */
+
+
+
+/**
+ * @swagger
+ * /clientes:
+ *   get:
+ *     summary: Retorna a lista de clientes
+ *     tags: [Clientes]
+ *     responses:
+ *       200:
+ *         description: Lista de clientes
+ *         content:
+ *           application/json:
+ *             example:
+ *               - id: 1
+ *                 nome: João
+ *                 sobreNome: Silva
+ *                 email: joaosilva@gmail.com
+ *                 endereco: Rua José Leite, 100, Centro, 12345-000, São Paulo, SP
+ *               - id: 2
+ *                 nome: Maria
+ *                 sobreNome: Santos
+ *                 email: mariasantos@gmail.com
+ *                 endereco: Rua José Leite, 100, Centro, 12345-000, São Paulo, SP
+ */
 clienteRoutes.get('/', clienteController.getAllClientes);
+
+/**
+ * @swagger
+ * /clientes/{id}:
+ *   get:
+ *     summary: Retorna um cliente pelo ID
+ *     tags: [Clientes]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: uuid
+ *         description: ID do cliente a ser retornado
+ *     responses:
+ *       200:
+ *         description: Cliente encontrado com sucesso
+ *         content:
+ *           application/json:
+ *             example:
+ *               id: 1
+ *               nome: João
+ *               sobreNome: Silva
+ *               email: joaosilva@gmail.com
+ *               endereco: Rua José Leite, 100, Centro, 12345-000, São Paulo, SP
+ *       404:
+ *         description: Cliente não encontrado
+ *       400:
+ *         description: Requisição inválida devido a validação
+ */
 clienteRoutes.get('/:id', validateGetById, checkValidationResultId, clienteController.getClienteById);
+
+/**
+ * @swagger
+ * /clientes:
+ *   post:
+ *     summary: Cria um novo cliente
+ *     tags: [Clientes]
+ *     requestBody:
+ *       description: Objeto do cliente a ser criado
+ *       required: true
+ *       content:
+ *         application/json:
+ *           example:
+ *             nome: João
+ *             sobrenome: Silva
+ *             email: joaosilva@gmail.com
+ *             endereco: Rua José Leite, 100, Centro, 12345-000, São Paulo, SP
+ *     responses:
+ *       201:
+ *         description: Cliente criado com sucesso
+ *         content:
+ *           application/json:
+ *             example:
+ *               id: 7edccea5-74a4-45ef-b7da-14be01d57bb0
+ *               nome: João
+ *               sobrenome: Silva
+ *               email: joaosilva@gmail.com
+ *               endereco: Rua José Leite, 100, Centro, 12345-000, São Paulo, SP        
+ *       400:
+ *         description: Requisição inválida devido a validação
+ */
 clienteRoutes.post('/', validateCliente, checkValidationResultCreate, clienteController.createCliente);
+
+
+/**
+ * @swagger
+ * /clientes/{id}:
+ *   put:
+ *     summary: Atualiza um cliente pelo ID
+ *     tags: [Clientes]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: uuid
+ *         description: ID do cliente a ser atualizado
+ *     requestBody:
+ *       description: Objeto do cliente com dados atualizados
+ *       required: true
+ *       content:
+ *         application/json:
+ *           example:
+ *             nome: Novo Nome
+ *             sobrenome: Novo Sobrenome
+ *             email: novoemail@gmail.com
+ *             endereco: Rua José Leite, 100, Centro, 12345-000, São Paulo, SP
+ *     responses:
+ *       200:
+ *         description: Cliente atualizado com sucesso
+ *         content:
+ *           application/json:
+ *             example:
+ *               id: 7edccea5-74a4-45ef-b7da-14be01d57bb0
+ *               nome: Novo Nome
+ *               sobrenome: Novo Sobrenome
+ *               email: novoemail@gmail.com
+ *               endereco: Rua José Leite, 100, Centro, 12345-000, São Paulo, SP
+ *       404:
+ *         description: Cliente não encontrado
+ *       400:
+ *         description: Requisição inválida devido a validação
+ */
 clienteRoutes.put('/:id', clienteController.updateCliente);
+
+/**
+ * @swagger
+ * /clientes/{id}:
+ *   delete:
+ *     summary: Exclui um cliente pelo ID
+ *     tags: [Clientes]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID do cliente a ser excluído
+ *     responses:
+ *       204:
+ *         description: Cliente excluído com sucesso
+ *       404:
+ *         description: Cliente não encontrado
+ *       400:
+ *         description: Requisição inválida devido à validação
+ */
 clienteRoutes.delete('/:id', validateGetById, checkValidationResultId, clienteController.deleteCliente);
 
 module.exports = clienteRoutes;
